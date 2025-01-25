6b3e3bdbe1394684a7c277a5bac36b22a35a0e64
Ichwan Noerfitrah <noerfiitrah02@gmail.com>
2025-01-25 10:48:42 +0700

    menggunakan package manager npm

--- NodeJs-Wpu/NPM/app.js
+++ NodeJs-Wpu/NPM/app.js
@@ -1,1 +1,17 @@
-console.log("Hello Ichwan!");
+// console.log("Hello Ichwan!");
+
+// fungsi validator adalah fungsi yang digunakan untuk melakukan validasi terhadap data yang kita punya
+
+const validator = require("validator");
+// const chalk = require("chalk");
+
+console.log(validator.isEmail("ichwan@gmail.com"));
+console.log(validator.isURL("https://www.google.com"));
+console.log(validator.isMobilePhone("081234567890", "id-ID"));
+
+// console.log(chalk.blue("Hello world!"));
+(async () => {
+  const chalk = await import("chalk");
+  console.log(chalk.default.bgBlue("Hello, World!"));
+  console.log(chalk.default.bgRed("Hello, World!"));
+})();
